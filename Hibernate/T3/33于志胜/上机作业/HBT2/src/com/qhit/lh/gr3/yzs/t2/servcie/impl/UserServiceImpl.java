e273c11749e54dc3f3a2a10ae7ff8f1ab6301b5c
yzs112233 <364930100@qq.com>
2017-09-05 20:18:02 +0800

    33于志胜提交Hibernate第三章上机作业

---
+++ Hibernate/T3/33于志胜/上机作业/HBT2/src/com/qhit/lh/gr3/yzs/t2/servcie/impl/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.qhit.lh.gr3.yzs.t2.servcie.impl;
+
+import java.util.List;
+
+import com.qhit.lh.gr3.yzs.t2.dao.BaseDao;
+import com.qhit.lh.gr3.yzs.t2.dao.impl.BaseDaoImpl;
+import com.qhit.lh.gr3.yzs.t2.servcie.UserService;
+
+public class UserServiceImpl implements UserService {
+	private BaseDao baseDao = new BaseDaoImpl();
+
+	@Override
+	public void add(Object obj) {
+		// TODO Auto-generated method stub
+		baseDao.add(obj);
+	}
+
+	@Override
+	public void delect(Object obj) {
+		// TODO Auto-generated method stub
+		baseDao.delect(obj);
+	}
+
+	@Override
+	public void update(Object obj) {
+		// TODO Auto-generated method stub
+		baseDao.update(obj);
+	}
+
+	@Override
+	public List<Object> getAll(String fromObject) {
+		// TODO Auto-generated method stub
+		return baseDao.getAll(fromObject);
+	}
+
+	@Override
+	public Object getObjectById(Class clazz, int id) {
+		// TODO Auto-generated method stub
+		return baseDao.getObjectById(clazz, id);
+	}
+
+}
---
+++ Hibernate/T3/33于志胜/上机作业/HBT2/test/com/qhit/lh/gr3/yzs/t2/DeptText.java
@@ -0,0 +1,50 @@
+package com.qhit.lh.gr3.yzs.t2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.qhit.lh.gr3.yzs.t2.bean.Dept;
+import com.qhit.lh.gr3.yzs.t2.bean.Emp;
+import com.qhit.lh.gr3.yzs.t2.servcie.UserService;
+import com.qhit.lh.gr3.yzs.t2.servcie.impl.UserServiceImpl;
+
+public class DeptText {
+private UserService userService = new UserServiceImpl();
+	@Test
+	public void add() {
+		Dept dept = new Dept();
+		dept.setDeptname("财政部");
+		dept.setAddress("漯河");
+		
+		Emp yzs = new Emp();
+		yzs.setEmpName("tom");
+		yzs.setEmpSex("男");
+		
+		
+		dept.getEmps().add(yzs);
+		
+		userService.add(dept);
+	}
+	@Test
+	public void delect(){
+		Dept dept = (Dept) userService.getObjectById(Dept.class, 1002);
+		userService.delect(dept);
+		
+	}
+	@Test
+	public void update(){
+		Dept dept = (Dept) userService.getObjectById(Dept.class, 1001);
+		dept.setDeptname("经济部");
+		userService.update(dept);
+	}
+	@Test
+	public void query(){
+		Dept dept = (Dept) userService.getObjectById(Dept.class, 1001);
+		for(Emp emp:dept.getEmps()){
+			System.out.println(dept.getDeptname()+":"+emp.getEmpName());
+		}
+		
+	}
+
+}
